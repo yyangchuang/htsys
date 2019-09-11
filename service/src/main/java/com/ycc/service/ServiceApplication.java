43a2f80c526e23cde8d02537ca376bd85ecf7508
875151694@qq.com <855151694@qq.com>
2019-09-11 21:57:50 +0800

    new start

---
+++ service/src/main/java/com/ycc/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ycc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
